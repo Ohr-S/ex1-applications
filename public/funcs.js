895be12d3d338b7b76093b0f0633c156461d434f
Ohr Shmueli <73228323+Ohr-S@users.noreply.github.com>
2022-05-09 11:51:47 +0300

    add jsGames buttons

--- public/funcs.js
+++ public/funcs.js
@@ -2,10 +2,29 @@
     document.addEventListener('DOMContentLoaded', function () {
 
         document.getElementById("example").addEventListener("click", example);
+        document.getElementById("qSyntax").addEventListener("click", qSyntax); 
+        document.getElementById("qObj").addEventListener("click", qObj); 
+        document.getElementById("qCalc").addEventListener("click", qCalc); 
+        document.getElementById("qAsync").addEventListener("click", qAsync);      
     }, false);
+    
 
    function example(){
-       alert("Button clicked");
-   }
+       alert("example");
+    }
+   function qSyntax(){
+    alert("syntax");
+    }
+    function qObj(){
+        alert("obj");
+    }
+    function qCalc(){
+        alert("calc");
+    }
+    function qAsync(){
+        alert("async");
+    }
+
+   
 
 })();
